7f5829c4c3189affe29ad3a4861ed341d1fc10c5
jpvernadet <jean-philippe.vernadet@laposte.net>
2019-09-17 11:01:51 +0200

    script cleaning

--- scripts/kraken_filter.py
+++ scripts/kraken_filter.py
@@ -3,10 +3,8 @@
 import sys, csv
 
 #script to filter kraken output 
-#only keep results with the best bitscore for each read
-#remove results with score below 50 and nb mismatch above 2 
+#only keep results with a taxid include in the list_id_strain (taxonomy_files)
 
-#reader=copen(sys.argv[1],"r"),delimiter="\t")
 list_id_strain = [taxid.rstrip('\n') for taxid in open(sys.argv[1],'r')]
 reader=csv.reader(open(sys.argv[2],"r"),delimiter="\t")
 output_path=sys.argv[3]
